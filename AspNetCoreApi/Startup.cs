caa8ae7e3e8aed5cb228fe1844dbefb18bc891be
ThiRO85 <thgr85@gmail.com>
2021-10-09 20:30:30 -0300

    Swagger

--- AspNetCoreApi/Startup.cs
+++ AspNetCoreApi/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace AspNetCoreApi
 {
@@ -21,6 +23,27 @@ namespace AspNetCoreApi
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "AspNetCoreCourse",
+                    Description = "Arquitetura DDD",
+                    //TermsOfService = new Uri(""),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Thiago Oliveira",
+                        Email = "thgr85@gmail.com"
+                        //Url = new Uri("")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de licença de uso"
+                        //Url = new Uri("")
+                    }
+                });
+                });
+
             services.AddControllers();
         }
 
@@ -30,6 +53,13 @@ namespace AspNetCoreApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "AspNetCoreCourse");
+                s.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
